b98dd26c161a702b4e0e2f2d3afa9a8c9467a289
Nice Friend <iliakashirin123@gmail.com>
2020-12-06 13:47:03 +0300

    lol

---
+++ nice friend2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main (){
+	setlocale(LC_ALL,"Russian");
+	int i;
+cout << "¬ведите число" << endl;
+string x,q;
+cin >> x;
+for(i=0;i<x.size();i++){
+ if(i%2!=1){
+ 	
+ 	
+cout << q << endl;
+	
+}else{
+
+	
+cout <<  x[i]	<< endl;
+
+	
+  } 
+ } 	
+}
